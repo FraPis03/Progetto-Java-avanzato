2deeb2ffa10184f9f33c9da888a10776542fe5ff
EmilioSolomita <emilio_solomita@libero.it>
2025-07-08 16:10:13 +0200

    Update Utente.java
    commenti per documentaizione
    
    Signed-off-by: EmilioSolomita <emilio_solomita@libero.it>

--- src/wordageddon/Utente.java
+++ src/wordageddon/Utente.java
@@ -7,7 +7,9 @@ package wordageddon;
  */
 
 /**
- *
+ * Classe che rappresenta un utente del sistema Wordageddon.
+ * Contiene le informazioni di autenticazione e metodi di validazione.
+ * 
  * @author antoniobellofatto
  */
 public class Utente {
@@ -16,37 +18,80 @@ public class Utente {
     private String password;
     private String email;
 
+    /**
+     * Costruttore con tutti i campi (nome utente, password, email).
+     * 
+     * @param nomeUtente Il nome utente
+     * @param password La password dell'utente
+     * @param email L'email dell'utente
+     */
     public Utente(String nomeUtente, String password, String email) {
         this.nomeUtente = nomeUtente;
         this.password = password;
         this.email = email;
     }
 
+    /**
+     * Costruttore con solo nome utente e password (senza email).
+     * 
+     * @param nomeUtente Il nome utente
+     * @param password La password
+     */
     public Utente(String nomeUtente, String password) {
         this.nomeUtente = nomeUtente;
         this.password = password;
     }
 
+    /**
+     * Restituisce il nome utente.
+     * 
+     * @return Il nome utente
+     */
     public String getNomeUtente() {
         return nomeUtente;
     }
 
+    /**
+     * Imposta un nuovo nome utente.
+     * 
+     * @param nomeUtente Il nuovo nome utente
+     */
     public void setNomeUtente(String nomeUtente) {
         this.nomeUtente = nomeUtente;
     }
 
+    /**
+     * Restituisce la password dell'utente.
+     * 
+     * @return La password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Imposta una nuova password per l'utente.
+     * 
+     * @param password La nuova password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Restituisce l'email dell'utente.
+     * 
+     * @return L'indirizzo email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Imposta una nuova email per l'utente.
+     * 
+     * @param email Il nuovo indirizzo email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
